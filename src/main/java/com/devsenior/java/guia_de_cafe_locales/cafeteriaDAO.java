79dba70e286a92220162d03cc5abb12311734d4a
luis miguel triana rueda <luistriana617@gmail.com>
2025-07-12 16:19:36 -0500

    Fix package declaration in Main class to match directory structure

--- src/main/java/com/devsenior/java/guia_de_cafe_locales/cafeteriaDAO.java
+++ src/main/java/com/devsenior/java/guia_de_cafe_locales/cafeteriaDAO.java
@@ -1,5 +1,24 @@
 package com.devsenior.java.guia_de_cafe_locales;
 
-public class cafeteriaDAO {
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
-}
+public class cafeteriaDAO {
+    /**
+     * este metodo se conecta a la db y obtiene todas las cafeterias 
+     * @return una lista de objetos cafeteria 
+     */
+
+     public List<cafeteria> obtenerTodas(){
+        List<cafeteria> cafeterias = new ArrayList<>();
+        String sql = " SELECT * FROM cafeterias";
+        try(Connection conn = DataBaseConnection.getConnection();
+        
+    ) {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+     }
+    }
