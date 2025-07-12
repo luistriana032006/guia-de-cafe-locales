fda1bf6aa9f05b54259a89639026d57b39be7d71
luis miguel triana rueda <luistriana617@gmail.com>
2025-07-11 21:05:07 -0500

    Enhance SQL view documentation for final report by adding comments for clarity

--- sql/union_de_tablas.sql
+++ sql/union_de_tablas.sql
@@ -2,13 +2,18 @@
 
 CREATE VIEW reporte_cafes as -- crear la tabla para hacer la visualización (permanente )
 select 
-    ca.nombre AS nombre_cafeteria, -- a la columna nombre_cafeteria de la
+    ca.nombre AS nombre_cafeteria, -- a la columna nombre_cafeteria de la 
     c.nombre AS nombre_cafe,
     c.precio
 FROM
-    cafeterias ca
+    cafeterias ca -- la tabla cafeteria tiene el sufijo ca para esta proyeccion del reporte de la base de datos 
 JOIN
-    cafe c ON ca.id_cafeteria = c.cafeteria_id;
+    cafe c ON ca.id_cafeteria = c.cafeteria_id; -- la tabla cafe tiene el sufijo c 
+    /* 
+    esto toma las dos tablas de cafe y cafeterias y las une UNICAMENTE
+    con las filas de la tabla c (cafes) donde el valor de la columna id_cafeteria
+    sea exactamente igual al valor de la columna cafeteria_id
+    */
 
 
 /* SELECT -- creara una vista temporal de la tabla 
