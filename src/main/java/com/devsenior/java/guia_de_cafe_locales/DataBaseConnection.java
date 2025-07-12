aa44370579a24eafcb1180407b9552688f1491a4
luis miguel triana rueda <luistriana617@gmail.com>
2025-07-11 22:45:11 -0500

    Update DataBaseConnection class to set the correct PostgreSQL password

--- src/main/java/com/devsenior/java/guia_de_cafe_locales/DataBaseConnection.java
+++ src/main/java/com/devsenior/java/guia_de_cafe_locales/DataBaseConnection.java
@@ -8,7 +8,7 @@ public class DataBaseConnection {
   // Reemplaza estos valores con los de tu base de datos
     private static final String URL = "jdbc:postgresql://localhost:5432/guia_cafes_db";
     private static final String USER = "postgres"; // Tu usuario de PostgreSQL
-    private static final String PASSWORD = "tu_contraseña"; // ¡CAMBIA ESTO!
+    private static final String PASSWORD = "admin123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
